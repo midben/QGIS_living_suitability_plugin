feaf495d9d18714458cb7dfdbb8079d1f822c4a3
c4028856@newcastle.ac.uk <c4028856@newcatle.ac.uk>
2026-08-27 10:55:45 +0100

    and __init__ oops

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
 def classFactory(iface):
-    """QGIS calls this function on plugin load. Must return the plugin class instance."""
     from .main import SuitabilityPlugin
     return SuitabilityPlugin(iface)
